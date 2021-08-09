7e872365f24f1e6ec92af0e0b3dc60f2b605a54d
William Hideki Kondo <willkondo@gmail.com>
2021-08-09 16:05:22 -0300

    Basic app layout

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,14 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText('Button');
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  test('it should render menu items', async () => {
+    render(<App />);
+
+    const apiMenuItem = await waitFor(() => screen.getByText('APIs'));
+    expect(apiMenuItem).toBeInTheDocument();
+
+    const favoriteMenuItem = screen.getByText('Favorites');
+    expect(favoriteMenuItem).toBeInTheDocument();
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
-import { Button } from 'antd';
+import { Layout, Menu } from 'antd';
 
 import 'antd/dist/antd.css';
 
-const App = () => (
-  <div className="App">
-    <Button type="primary">Button</Button>
-  </div>
-);
+import APP_NAME from './config/app_name';
+
+const { Header, Content, Footer } = Layout;
+
+const App = () => {
+  return (
+    <Layout style={{ minHeight: '100vh' }}>
+      <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+          <Menu.Item key="1">APIs</Menu.Item>
+          <Menu.Item key="2">Favorites</Menu.Item>
+        </Menu>
+      </Header>
+
+      <Content style={{ padding: '0 50px', marginTop: 64 }}>
+        <div style={{ padding: 24, minHeight: '100%' }}>Content</div>
+      </Content>
+
+      <Footer style={{ textAlign: 'center' }}>{APP_NAME} ©2021</Footer>
+    </Layout>
+  );
+};
 
 export default App;
