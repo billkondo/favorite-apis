8ac26ad6724f4904e834939231fc78369b6332f5
William Hideki Kondo <willkondo@gmail.com>
2021-08-12 16:52:18 -0300

    Adding global error handler

--- src/index.tsx
+++ src/index.tsx
@@ -28,6 +28,11 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+window.onunhandledrejection = (error: PromiseRejectionEvent) => {
+  // Use this part for logging/tracking errors
+  console.error(error);
+};
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
