9d0f76a037ddcbe4bf07aca602d8aa21fe7cb6fe
William Hideki Kondo <willkondo@gmail.com>
2021-08-13 23:18:39 -0300

    Configuring services injection

---
+++ src/config/environment.ts
@@ -0,0 +1,2 @@
+export const LOCAL = process.env.REACT_APP_LOCAL === 'true';
+export const EMULATOR = process.env.REACT_APP_EMULATOR === 'true';
--- src/services/firebase/index.ts
+++ src/services/firebase/index.ts
@@ -3,6 +3,7 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/functions';
 
+import { EMULATOR } from 'config/environment';
 import config from 'config/firebase.json';
 
 firebase.initializeApp(config);
@@ -10,7 +11,7 @@ firebase.initializeApp(config);
 export const auth = firebase.auth();
 export const functions = firebase.functions();
 
-if (process.env.REACT_APP_EMULATOR) {
+if (EMULATOR) {
   auth.useEmulator('http://localhost:9099');
   functions.useEmulator('localhost', 5001);
 }
