c8ef0e9eb78a4e946bc5425d3408c5ab85f28354
William Hideki Kondo <willkondo@gmail.com>
2021-08-13 23:04:37 -0300

    Implementing favorite services integrated with Firebase

--- src/services/firebase/index.ts
+++ src/services/firebase/index.ts
@@ -1,15 +1,21 @@
 import firebase from 'firebase/app';
 
 import 'firebase/auth';
+import 'firebase/functions';
 
 import config from 'config/firebase.json';
 
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
+export const functions = firebase.functions();
 
 if (process.env.REACT_APP_EMULATOR) {
   auth.useEmulator('http://localhost:9099');
+  functions.useEmulator('localhost', 5001);
 }
+
+export const listFunction = functions.httpsCallable('list');
+export const favoriteFunction = functions.httpsCallable('favorite');
 
 export {};
